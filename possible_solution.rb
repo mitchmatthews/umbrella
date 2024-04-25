9ab97b387db98d01422cec279f380cf4797f7494
Mitch Matthews <165244888+mitchmatthews@users.noreply.github.com>
2024-04-25 20:51:04 +0000

    Completed

--- possible_solution.rb
+++ possible_solution.rb
@@ -20,42 +20,26 @@ gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=#{user_lo
 
 # p "Getting coordinates from:"
 # p gmaps_url
-
 raw_gmaps_data = HTTP.get(gmaps_url)
-
 parsed_gmaps_data = JSON.parse(raw_gmaps_data)
-
 results_array = parsed_gmaps_data.fetch("results")
-
 first_result_hash = results_array.at(0)
-
 geometry_hash = first_result_hash.fetch("geometry")
-
 location_hash = geometry_hash.fetch("location")
-
 latitude = location_hash.fetch("lat")
-
 longitude = location_hash.fetch("lng")
-
 puts "Your coordinates are #{latitude}, #{longitude}."
 
 # Get the weather from Pirate Weather API
-
 pirate_weather_key = ENV.fetch("PIRATE_WEATHER_KEY")
-
 pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_key}/#{latitude},#{longitude}"
 
 # p "Getting weather from:"
 # p pirate_weather_url
-
 raw_pirate_weather_data = HTTP.get(pirate_weather_url)
-
 parsed_pirate_weather_data = JSON.parse(raw_pirate_weather_data)
-
 currently_hash = parsed_pirate_weather_data.fetch("currently")
-
 current_temp = currently_hash.fetch("temperature")
-
 puts "It is currently #{current_temp}°F."
 
 # Some locations around the world do not come with minutely data.
@@ -63,32 +47,22 @@ minutely_hash = parsed_pirate_weather_data.fetch("minutely", false)
 
 if minutely_hash
   next_hour_summary = minutely_hash.fetch("summary")
-
   puts "Next hour: #{next_hour_summary}"
 end
 
 hourly_hash = parsed_pirate_weather_data.fetch("hourly")
-
 hourly_data_array = hourly_hash.fetch("data")
-
 next_twelve_hours = hourly_data_array[1..12]
-
 precip_prob_threshold = 0.10
-
 any_precipitation = false
-
 next_twelve_hours.each do |hour_hash|
   precip_prob = hour_hash.fetch("precipProbability")
 
   if precip_prob > precip_prob_threshold
     any_precipitation = true
-
     precip_time = Time.at(hour_hash.fetch("time"))
-
     seconds_from_now = precip_time - Time.now
-
     hours_from_now = seconds_from_now / 60 / 60
-
     puts "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of precipitation."
   end
 end
